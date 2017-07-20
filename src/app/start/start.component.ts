d09f5757cb2f7da1a1f0e83939d4b27f74fbce7d
raiff.mello@splab.ufcg.edu.br <raiffzzz@gmail.com>
2017-07-20 11:12:55 -0300

    added material angular icons

--- src/app/start/start.component.ts
+++ src/app/start/start.component.ts
@@ -9,13 +9,19 @@ export class StartComponent implements OnInit {
 
   socialNetwork: string;
   dataToSend: string;
+  star = false;
 
   constructor() {
     this.socialNetwork = 'musician social network';
     this.dataToSend = 'data from server';
+    this.star = true;
   }
 
   ngOnInit() {
+  }
+
+  onClick() {
+    this.star = !this.star;
   }
 
 }
